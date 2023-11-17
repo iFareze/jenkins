afb2481a6747fe99f3fddff0fd048dd77260dddd
Adil Gheziel <71925662+iFareze@users.noreply.github.com>
2023-11-17 11:36:31 +0000

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,9 +7,7 @@ terraform {
   }
 }
 provider "docker" {
-  host     = "unix:///var/run/docker.sock"
-  #host     = "ssh://root@10.0.2.15"
+  #host     = "unix:///var/run/docker.sock"
+  host     = "ssh://root@10.0.2.15"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
-
-
 }
